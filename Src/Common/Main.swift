9153ad6b75cba004a7ba843fb188df9e007769e9
Maxim Khatskevich <maxim@khatskevi.ch>
2016-08-21 21:13:36 +0100

    Initial complete version
    Maintains weak registry of all entity wrappers currently in memeory, as weak references, so no need to “unregister” manually.

---
+++ Src/Common/Main.swift
@@ -0,0 +1,147 @@
+//
+//  Main.swift
+//  MKHEntityManager
+//
+//  Created by Maxim Khatskevich on 08/18/16.
+//  Copyright © 2016 Maxim Khatskevich. All rights reserved.
+//
+
+public
+typealias GlobalEntityId = String
+
+//===
+
+public
+typealias EntityId = String
+
+//===
+
+public
+protocol IdentifiableEntity
+{
+    var enm_entityId: EntityId { get }
+}
+//===
+
+fileprivate
+func getGlobalId<T: IdentifiableEntity>(for entity: T) -> GlobalEntityId
+{
+    // system-wide unique key
+    
+    return "\(String(reflecting: type(of: entity)))::\(entity.enm_entityId)"
+}
+
+//===
+
+public
+protocol Entity: IdentifiableEntity
+{
+    var enm_globalId: GlobalEntityId { get }
+}
+
+public
+extension Entity
+{
+    var enm_globalId: GlobalEntityId
+    {
+        return getGlobalId(for: self)
+    }
+}
+
+//===
+
+public
+struct Weak<T: AnyObject>
+{
+    public private(set)
+    weak var value: T?
+    
+    public
+    init(_ value: T?)
+    {
+        self.value = value
+    }
+}
+
+//===
+
+public
+enum EntityManager
+{
+    private
+    static
+    var wrappers: [Weak<AnyObject>] = []
+    
+    //===
+    
+    public
+    static
+    func wrap<T: Entity>(_ entity: T) -> EntityWrapper<T>
+    {
+        // cleanup first
+        
+        wrappers = wrappers.filter({ $0.value != nil })
+        
+        //===
+        
+        if
+            let existingWrapper =
+                wrappers
+                    .flatMap({ $0.value as? Entity })
+                    .filter({ $0.enm_globalId == entity.enm_globalId })
+                    .flatMap({ $0 as? EntityWrapper<T>})
+                    .first
+        {
+            existingWrapper.entity = entity // update entity
+            
+            //===
+            
+            return existingWrapper
+        }
+        else
+        {
+            let newWrapper = EntityWrapper(entity)
+            
+            //===
+            
+            wrappers.append(
+                Weak(newWrapper))
+            
+            //===
+            
+            return newWrapper
+        }
+    }
+}
+
+//===
+
+public
+final
+class EntityWrapper<T: Entity>: Entity
+{
+    // Entity is recommendet to be immutable
+    
+    public
+    var enm_globalId: GlobalEntityId
+    {
+        return entity.enm_globalId
+    }
+    
+    public
+    var enm_entityId: EntityId
+    {
+        return entity.enm_entityId
+    }
+    
+    public
+    var entity: T
+    
+    //===
+    
+    fileprivate
+    init(_ entity: T)
+    {
+        self.entity = entity
+    }
+}
---
+++ Tst/Main.swift
@@ -0,0 +1,38 @@
+//
+//  Main.swift
+//  MKHEntityManagerTst
+//
+//  Created by Maxim Khatskevich on 8/18/16.
+//  Copyright © 2016 Maxim Khatskevich. All rights reserved.
+//
+
+import XCTest
+import MKHEntityManager
+
+//===
+
+class Main: XCTestCase
+{
+    struct Person: Entity
+    {
+        let enm_entityId: EntityId
+    }
+    
+    //===
+    
+    func testEntityWrapperReusability()
+    {
+        let p = Person(enm_entityId: "1")
+        let anotherP = Person(enm_entityId: "2")
+        
+        let w1 = EntityManager.wrap(p)
+        let w2 = EntityManager.wrap(p)
+        let w3 = EntityManager.wrap(anotherP)
+        
+        XCTAssert(w1 === w2)
+        XCTAssert(w1.entity.enm_entityId == w2.entity.enm_entityId)
+        
+        XCTAssert(w2 !== w3)
+        XCTAssert(w2.entity.enm_entityId != w3.entity.enm_entityId)
+    }
+}
